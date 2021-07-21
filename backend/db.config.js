0f3771149f53a00bf6d3e5d95febb206124fa637
JolaMeissner <j.meissner43@gmail.com>
2021-07-21 16:14:33 +0200

    Projektabgabe

---
+++ backend/db.config.js
@@ -0,0 +1,26 @@
+/*
+ * eine der beiden folgenden dbConfig-Objekte muessen auskommentiert werden!
+ */
+
+/*
+ * Konfigurationsdaten fuer die Verbindung mit localhost
+ */
+/*
+export const dbConfig = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "IhrPasswort",
+    DB: "posts"
+};
+
+/*
+ * Konfigurationsdaten fuer die Verbindung mit studi.f4.htw-berlin.de
+ */
+
+export const dbConfig = {
+    HOST: "db.f4.htw-berlin.de",
+    USER: "s0563985",
+    PASSWORD: "restarT99",
+    DB: "_s0563985__posts2"
+};
+
